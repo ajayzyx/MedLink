882d022bb0118840c552c104c2f171ce04c14bed
Ajay singh raghuwanshi <ajay1c4002@gmail.com>
2025-01-15 16:41:07 +0530

    lower section update

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,187 @@
+import React from 'react'
+import MedLinkLogo from '../assets/images/medlinklogo.png'
+import fb1 from '../assets/images/fb1.png'
+import Insta1 from '../assets/images/Insta1.png'
+import Mail from '../assets/images/Mail.png'
+import Whatsapp from '../assets/images/Whatsapp.png'
+import X from '../assets/images/X.png'
+import DigifrillsLogo from '../assets/images/DigifrillsLogo.png'
+import Footer1 from '../assets/images/Footer1.png'
+import Foot1 from '../assets/images/Foot1.svg'
+import Foot2 from '../assets/images/Foot2.svg'
+import Foot3 from '../assets/images/Foot3.svg'
+import Foot4 from '../assets/images/Foot4.svg'
+import Foot5 from '../assets/images/Foot5.svg'
+
+function Footer() {
+return (
+<>
+<div className="flex flex-col">
+    <div className='bg-white w-full min-h-[540px] px-12 flex justify-between items-center flex-col'>
+        <div className='max-w-[1392px]  max-h-[420px] bg-[#F2F2F2]  rounded-2xl flex'>
+            <div className='max-w-[720px] flex-row p-16 gap-4'>
+                <h4 className='text-5xl text-[#2A2A2A] font-bold'>
+                    Transform Your Health Journey with <span
+                        className='text-5xl text-[#0065FB] font-bold'>MedLinkPro</span>
+                </h4>
+                <p className='text-[#737373] text-lg font-normal my-8'>Manage your health smarter with
+                    MedLinkPro—prescriptions, records, services, and recommendations, all in one place. Transforming
+                    healthcare for a better future.</p>
+                <button className='text-white rounded-xl bg-[#0065FB] w-full text-lg p-2'>Get Started Now</button>
+            </div>
+            <div className='flex-row w-[700px] h-[420px] relative'>
+                <img src={Footer1} alt="" className='w-[700px] absolute bottom-4 right-8' />
+                <img src={Foot1} alt="" className='absolute w-16 left-4 top-16'/>
+                <img src={Foot2} alt="" className='absolute w-8 right-80 bottom-8'/>
+                <img src={Foot3} alt="" className='absolute w-8 top-14 left-48'/>
+                <img src={Foot4} alt="" className='absolute w-28 right-32 bottom-28'/>
+                <img src={Foot5} alt="" className='absolute w-8 right-12 top-40'/>
+            </div>
+        </div>
+    </div>
+    <div className='flex flex-col'>
+        <div className="mb-8 md:mb-0 flex pb-6  w-full items-center justify-between px-12 ">
+            <div>
+                <h2 className="text-lg font-semibold">Join our newsletter</h2>
+                <p className="text-gray-600 mt-1">
+                    Get updates from us weekly about project management.
+                </p>
+            </div>
+            <div className="mt-4 flex">
+                <input type="email" placeholder="Enter your email"
+                    className="py-2 px-6 border rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none" />
+                <div className="ml-8 rounded-xl">
+                    <button className="bg-blue-600 rounded-xl text-white py-3 px-6  hover:bg-blue-700">
+                        Subscribe
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className='border-t border-gray-200 pt-8 flex justify-between items-start mx-10'></div>
+        <div className='flex px-12'>
+        <div className=' flex flex-row basis-4/6'>
+          <div className="text-left flex-col flex">
+            <div className="flex items-center flex-row">
+                <img src={MedLinkLogo} alt="" className="size-9" />
+                <h3 className="font-semibold text-2xl ml-3">Medlink</h3>
+            </div>
+            <p className="text-xl font-medium text-[#2B2B2B] mt-6">
+                Address:
+            </p>
+            <p className="text-lg text-[#737373] font-normal">
+                block no, street name, road name, Gujarat 90783
+            </p>
+            <p className="text-xl font-medium text-[#2B2B2B] mt-6">Contact:</p>
+            <p className="text-lg text-[#737373] font-normal mt-3"> 9389218327</p>
+            <p className="text-lg text-[#737373] font-normal m-0">medlinkpro@gmail.com</p>
+            <div className="flex justify-center md:justify-start space-x-4">
+                <a href="#" className="text-gray-600 hover:text-blue-600">
+                    <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600">
+                    <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600">
+                    <i className="fab fa-instagram"></i>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600">
+                    <i className="fab fa-linkedin-in"></i>
+                </a>
+            </div>
+            </div>
+        </div>
+        <div className='flex flex-row justify-center items-center'>
+          <div className=''>
+          <div className="flex gap-48 mt-5 text-left">
+              <div className="text-center">
+                <h3 className="font-semibold text-lg">Sitemap</h3>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Features
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Project
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Pricing
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="text-center">
+                <h3 className="font-semibold text-lg">Company</h3>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Careers
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Contact Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-gray-600 hover:underline">
+                      Blog
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div className="flex justify-between items-center px-12">
+                <div className="flex gap-6">
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={fb1} alt="" className="h-6" />
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Insta1} alt="" className="h-6" />
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={X} alt="" className="h-6" />
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Whatsapp} alt="" className="h-6" />
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={Mail} alt="" className="h-6" />
+                    </a>
+                </div>
+
+                <div>
+                    <img src={DigifrillsLogo} alt="" className="h-14" />
+                </div>
+            </div>
+             {/* Footer Bottom */}
+             <div className="mt-10 border-t pt-2">
+              <p className='mx-12 m-4 text-[#434344] text-base font-medium'>
+                &copy; 2024 MedLinkPro. All Rights Reserved.
+              </p>
+            </div>
+
+    </div>
+</>
+
+)
+}
+
+export default Footer
---
+++ src/pages/Questions.jsx
@@ -0,0 +1,88 @@
+import React,{useState} from 'react'
+import { motion } from 'framer-motion';
+export const Questions = () => {
+const [activeQuestion, setActiveQuestion] = useState(null);
+
+const questions = [
+{
+id: 1,
+question: "What is MedLinkPro?",
+answer: "MedLinkPro is a platform that simplifies patient management and reduces administrative tasks for healthcare professionals.",
+},
+{
+id: 2,
+question: "How does it improve the patient experience?",
+answer: "Enables patients to communicate directly with healthcare providers for quick clarifications. Ensures patient data is kept private and secure.",
+},
+{
+id: 3,
+question: "How do digital prescriptions work?",
+answer: "Digital prescriptions not only make the process more efficient but also enhance patient safety by reducing medication errors and ensuring better documentation.",
+},
+{
+id: 4,
+question: "Can you use MedLinkPro while traveling?",
+answer: "Yes, MedLinkPro is designed to be accessible anytime, anywhere, making it highly convenient for use while traveling.",
+},
+{
+id: 5,
+question: "Who is MedLinkPro for?",
+answer: "MedLinkPro is a versatile solution tailored for healthcare providers, patients, and administrators, making healthcare more efficient.",
+},
+{
+id: 6,
+question: "How can you start using MedLinkPro?",
+answer: "Whether you're a healthcare provider or a patient, starting with MedLinkPro takes just a few simple steps.",
+},
+];
+
+const toggleAnswer = (id) => {
+setActiveQuestion(activeQuestion === id ? null : id);
+};
+
+
+return (
+<div className="m-20">
+    <div className="bg-[#FFFFFF] m-8 flex flex-col items-center justify-center">
+        <h3 className="p-8 text-5xl font-semibold">
+            Smart Question Always Asked
+        </h3>
+        <p className="w-[720px] text-[#737373] text-center">
+            Get answers to the most common inquiries about MedLinkPro, designed to help you understand how our solutions
+            can improve your healthcare experience.
+        </p>
+    </div>
+    <div className="flex flex-col items-center justify-center gap-y-8">
+        {questions.map(({id, question, answer}) => (
+        <div key={id} className="border flex-col inline-flex items-center justify-center px-6 py-2 rounded-lg shadow-sm cursor-pointer w-[1100px] min-h-[132px] bg-[#F7F7F7]"
+            onClick={()=> toggleAnswer(id)}
+            >
+            <div className="flex w-full justify-between items-center">
+                <h3 className="text-3xl font-medium align-baseline">{question}</h3>
+                <div className={`inline-flex items-center justify-center transform-gpu duration-500 ${activeQuestion === id ? 'rotate-180' : ''}`}>
+                    <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.4"
+                        stroke="currentColor" className="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                    </motion.svg>
+                </div>
+              
+            </div>
+           
+                <motion.p
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={activeQuestion === id ? { height: "80px", opacity: 1 } : { height: 0, opacity: 0 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{ duration: 0.6, ease: "easeInOut" }}
+                  className="py-2 text-2xl text-gray-600 overflow-hidden"
+                >
+                  {answer}
+                </motion.p>
+              
+        </div>
+        ))}
+    </div>
+</div>
+)
+}
+
+export default Questions
