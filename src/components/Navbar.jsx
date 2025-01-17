d62c3702940af479441b01b442858565fb252559
Ajay singh raghuwanshi <ajay1c4002@gmail.com>
2025-01-17 18:50:58 +0530

    responsiveness updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import MedLinkLogo from "../assets/images/medlinklogo.png"
 
 function Navbar() {
   return (
-    <nav className ="bg-[#FFFFFF]  fixed top-0 right-0 z-40 w-full  px-6 py-4 shadow-lg lg:px-12">
+    <nav className ="bg-[#FFFFFF]  fixed top-0 right-0 z-40 w-full  px-6 py-4 shadow-lg lg:px-16">
         <div className="flex gap-2  items-center w-full justify-between lg:h-16">
-            <div className="inline-flex gap-2 justify-center items-center">
+            <div className="inline-flex gap-4 justify-center items-center">
                 <div className='inline-flex  items-center size-12 justify-center'>
                     <img src={MedLinkLogo} alt ="Logo" className="size-12 " />
                 </div>
-                <span className=" text-xl font-bold font-oakes">Medlink</span>
+                <span className="text-2xl font-bold font-oakes text-[#2A2A2A]">Medlink</span>
             </div>
-            <div className=" w-full md:flex md:max-w-[400px] lg:max-w-[500px] lg:text-xl flex-nowrap  items-center justify-between hidden  text-lg font-normal font-oakes">
+            <div className=" w-full md:flex md:max-w-[400px] lg:max-w-[500px] flex-nowrap  items-center justify-between hidden md:text-base lg:text-lg font-normal font-oakes text-[#434344]">
                 <div className='inline-flex items-center justify-center'>
                     <a href="">Home</a>
                     </div>
@@ -28,8 +28,8 @@ function Navbar() {
                     <a href="">Contact</a>
                     </div>
             </div>
-            <div className='items-center inline-flex justify-center font-oakes'>
-                <button className="py-3 text-nowrap font-medium text-white rounded-xl bg-[#0065FB] px-4  hover:bg-blue-700 ">
+            <div className='items-center inline-flex justify-center font-sofia text-lg'>
+                <button className="lg:py-3 text-nowrap font-medium text-white rounded-xl bg-[#0065FB] lg:px-4 py-2 px-3 hover:bg-blue-700 ">
                     Get Started
                 </button>
             </div>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -6,11 +6,11 @@ return (
 <div className='w-full '>
     <div className="pt-6 md:mt-28 lg:mt-32 md:mx-10 mx-4 mt-24 bg-[#EEEEEE] rounded-2xl ">
         <div className="flex items-center justify-center w-full bg-[#EEEEEE]">
-            <div className="inline-flex flex-col items-center justify-center md:p-12 p-4 bg-[#EEEEEE]">
+            <div className="inline-flex flex-col items-center justify-center md:px-12 md:pt-12 md:pb-6 p-4 bg-[#EEEEEE]">
                 <h3 className=" font-bold text-center font-oakes md:text-5xl lg:text-7xl text-2xl gap-y-1">
                     One Window, One Step <br /> <span className="text-[#0065FB]">Total Health Solution</span>
                 </h3>
-                <div className='md:pt-10 pt-6'>
+                <div className='md:pt-12 pt-6'>
                     <p className="max-w-[264px] md:max-w-[420px] text-center font-oakes font-normal text-sm tracking-tight text-[#737373]">
                         The website will act as a hub to connect the MedLinkPro app with hospital systems,
                         making it easier for patients, doctors, and hospitals to share and access healthcare
@@ -21,7 +21,7 @@ return (
         </div>
         <div className ="flex items-center justify-center">
             <button
-                className ="bg-[#0065FB] shadow-lg shadow-blue-500/50 py-4 px-6  text-[#EEEEEE] font-sofia font-semibold rounded-xl">
+                className ="bg-[#0065FB] py-4 px-6  text-[#EEEEEE] font-sofia font-semibold rounded-xl" style={{ boxShadow: '0px 12px 48px 0px rgba(1, 107, 252, 0.32)'}}>
                 Request a Demo
             </button>
             <button className="py-4 px-6 rounded-lg text-[#000000] font-sofia font-semibold bg-white ml-4">
@@ -31,13 +31,13 @@ return (
         <div className="relative h-[744px] w-full ">
             <div className="absolute bottom-0 left-14">
                 <div className="inline-flex items-center justify-center">
-                    <img className="w-[1296px] style={{ boxShadow: '0px 12px 48px 0px rgba(161, 161, 161, 0.25)' }}" src={HeroIMG} alt="Hero" />
+                    <img className="lg:w-[1296px] style={{ boxShadow: '0px 12px 48px 0px rgba(161, 161, 161, 0.25)' }} md:" src={HeroIMG} alt="Hero" />
                 </div>
             </div>
 
             <div className="absolute bottom-0 right-16">
                 <div className="inline-flex items-center justify-center">
-                    <img className="h-auto w-[316px]" src={HeroPhone} />
+                    <img className="h-auto lg:w-[316px] hidden" src={HeroPhone} />
                 </div>
             </div>
             <div className="absolute z-40 bottom-0 left-0 right-0 h-64 bg-gradient-to-t from-gray-50 to-transparent">
